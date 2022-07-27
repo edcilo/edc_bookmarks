7ad48be9efe798ef1a30648aedafb191b0ce1496
edcilo <edcilop@gmail.com>
2022-07-27 11:54:11 -0500

    add serializers

--- ms/serializers/resourceSerializer.py
+++ ms/serializers/resourceSerializer.py
@@ -2,8 +2,10 @@ from ms.helpers import time
 from ms.serializers import Serializer
 
 
-class <CLASSNAME>(Serializer):
+class ResourceSerializer(Serializer):
     response = {
         "id": str,
+        "name": str,
+        "url": str,
         "created_at": time.datetime_to_epoch,
     }
---
+++ ms/serializers/workspaceSerializer.py
@@ -0,0 +1,12 @@
+from ms.helpers import time
+from ms.serializers import Serializer
+
+
+class WorkspaceSerializer(Serializer):
+    response = {
+        "id": str,
+        "name": str,
+        "description": str,
+        "created_at": time.datetime_to_epoch,
+        "deleted_at": lambda date: time.datetime_to_epoch(date) if date else None,
+    }
